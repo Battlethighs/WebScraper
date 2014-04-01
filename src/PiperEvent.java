c65c41e8429f5500dad4c8125f687a14e2f4c696
Andrew <ahansen4@macalester.edu>
2014-04-01 02:15:59 -0500

    New scraper in progress

--- src/PiperEvent.java
+++ src/PiperEvent.java
@@ -1,99 +1,45 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PiperEvent {
 
     private boolean hasFood;
-    private String title;
-    private String time;
-    private String place;
-    private String description;
-    private List<String> foodWords = new LinkedList<String>();
-    private List<String> placeWords = new LinkedList<String>();
-    private List<String> timeWords = new LinkedList<String>();
+    private String title = "";
+    private String time = "";
+    private String place = "";
+    private String description = "";
+    //TODO: Deal w words like 'teacher' erroneously returning TRUE because of 'tea' substring and shit like that.
+    private String foodRegex = "[Bb]reakfast|[Ll]unch|[Dd]inner|[Ss]nack|[Rr]efreshments|[Pp]ie|[Cc](ake|offee|hocolate|andy)|[Ii]ce cream]|[Tt]ea|[Dd]rinks|[Pp]izza]";
+    //TODO: Add accommodation for room numnbers like "HUM204" or "OLRI150"
+    private String placeRegex = "[Oo]lin[ -][Rr]ice|[Cc]ampus[ -][Cc]enter|[Cc]arnegie|[Oo]ld [Mm]ain|[Mm]arkim [Hh]all|[Kk]agin|GSRC";
+    //TODO: Add RegEx for times written w only 1 digit (e.g. '5 p.m.' or '1 o'clock')
+    private String timeRegex = "[Nn]oon|[0-9]:[0-9][0-9]";
 
     public PiperEvent (String title, String body){
 
         this.title = title;
-        //TODO add case sensitivity
-        foodWords.add("[Bb]reakfast");
-        foodWords.add("[Ll]unch");
-        foodWords.add("[Dd]inner");
-        foodWords.add("[Ss]nack");
-        foodWords.add("[Rr]efreshments");
-        foodWords.add("[Pp]ie ");
-        foodWords.add("[Cc]ake");
-        foodWords.add("ice cream");
-        foodWords.add("[Tt]ea");
-        foodWords.add("[Cc]offee");
-        foodWords.add("[Cc]hocolate");
-        foodWords.add("[Cc]andy");
-        foodWords.add("[Dd]rinks");
-        foodWords.add("[Pp]izza");
+        String sentenceEndRegex = ("(?<=[\\.\\!\\?]) (?=[A-Z0-9])");
+        String sentences[] = body.split(sentenceEndRegex);
+        Pattern FOOD_PATTERN = Pattern.compile(foodRegex);
+        Pattern PLACE_PATTERN = Pattern.compile(placeRegex);
+        Pattern TIME_PATTERN = Pattern.compile(timeRegex);
 
-        placeWords.add("[Oo]lin-Rice");
-        placeWords.add("Carnegie");
-        placeWords.add("[Cc]ampus [Cc]enter");
-
-        timeWords.add("noon");
-        timeWords.add("[0-9][\\:][0-9][0-9]");
-        timeWords.add("[0-9] [pa]\\.m");
-
-        //TODO add regex for other forms of punctuation
-        String[] sentences = body.split("\\.");
-
-        for ( String foodWord : foodWords ){
-            boolean breakFlag = false;
-            for ( String sentence : sentences ){
-                String[] words = sentence.split(" ");
-                for ( String word : words){
-                    if(word.matches(foodWord)){
-                    System.out.println(foodWord);
-                        this.hasFood = true;
-                        this.description = sentence;
-                        breakFlag = true;
-                        break;
-                    }
-                }
-            }
-            if (breakFlag) {
-                break;
-            }
-            this.hasFood = false;
-            this.description = null;
-            this.time = null;
-            this.place = null;
-        }
-
-        if ( hasFood ){
-            for ( String placeWord : placeWords  ){
-                boolean breakFlag = false;
-                for ( String sentence : sentences ){
-                    if ( sentence.contains(placeWord) ){
-                        this.place = sentence;
-                        breakFlag = true;
-                        break;
-                    }
-                }
-                if (breakFlag){
-                    break;
-                }
-            }
-
-            for (String timeWord : timeWords){
-                boolean breakFlag = false;
-                for (String sentence : sentences){
-                    if (sentence.contains(timeWord)){
-                        this.time = sentence;
-                        breakFlag = true;
-                        break;
-                    }
-                }
-                if (breakFlag){
-                    break;
-                }
+        for(String sentence : sentences){
+            Matcher f = FOOD_PATTERN.matcher(sentence);
+            Matcher p = PLACE_PATTERN.matcher(sentence);
+            Matcher t = TIME_PATTERN.matcher(sentence);
+            if (f.find()){
+                this.hasFood = true;
+                this.description = f.group(0);
+            }if (p.find()){
+                this.place = p.group(0);
+            }if (t.find()){
+                this.time = t.group(0);
             }
         }
+
     }
 
     public boolean getHasFood() { return hasFood; }
